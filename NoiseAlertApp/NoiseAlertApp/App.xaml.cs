f499f5ca57d65467e55e92337494354daf3a7033
makhtar18 <122763616+makhtar18@users.noreply.github.com>
2023-05-20 20:46:36 -0700

    Initial MAUI Template

---
+++ NoiseAlertApp/NoiseAlertApp/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace NoiseAlertApp;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
+
---
+++ NoiseAlertApp/NoiseAlertApp/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace NoiseAlertApp;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
+
