044d55288eb03e9176cc9afd1a7b9e1747ee722b
makhtar18 <122763616+makhtar18@users.noreply.github.com>
2023-05-23 13:49:54 -0700

    Revert "Undoing Merge"
    This reverts commit e7e75beb32612d3cd1fe6ce5c5b25efe03896a37.

--- NoiseAlertApp/NoiseAlertApp/MainPage.xaml.cs
+++ NoiseAlertApp/NoiseAlertApp/MainPage.xaml.cs
@@ -1,16 +1,25 @@
-﻿using NoiseAlertApp.ViewModels;
-
-namespace NoiseAlertApp;
+﻿namespace NoiseAlertApp;
 
 public partial class MainPage : ContentPage
 {
+	int count = 0;
 
-	public MainPage(MainViewModel vm)
+	public MainPage()
 	{
 		InitializeComponent();
-        BindingContext = vm;
-    }
+	}
 
+	private void OnCounterClicked(object sender, EventArgs e)
+	{
+		count++;
+
+		if (count == 1)
+			CounterBtn.Text = $"Clicked {count} time";
+		else
+			CounterBtn.Text = $"Clicked {count} times";
+
+		SemanticScreenReader.Announce(CounterBtn.Text);
+	}
 }
 
 
--- NoiseAlertApp/NoiseAlertApp/MauiProgram.cs
+++ NoiseAlertApp/NoiseAlertApp/MauiProgram.cs
@@ -1,6 +1,5 @@
 ﻿using Microsoft.Extensions.Logging;
-using NoiseAlertApp.ViewModels;
-using Syncfusion.Maui.Core.Hosting;
+
 namespace NoiseAlertApp;
 
 public static class MauiProgram
@@ -10,17 +9,14 @@ public static class MauiProgram
 		var builder = MauiApp.CreateBuilder();
 		builder
 			.UseMauiApp<App>()
-			.ConfigureSyncfusionCore()
 			.ConfigureFonts(fonts =>
 			{
 				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
 				fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
 			});
-        builder.Services.AddSingleton<MainPage>();
-        builder.Services.AddSingleton<MainViewModel>();
 
 #if DEBUG
-        builder.Logging.AddDebug();
+		builder.Logging.AddDebug();
 #endif
 
 		return builder.Build();
