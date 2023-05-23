e7e75beb32612d3cd1fe6ce5c5b25efe03896a37
makhtar18 <122763616+makhtar18@users.noreply.github.com>
2023-05-23 13:46:11 -0700

    Undoing Merge

--- NoiseAlertApp/NoiseAlertApp/MauiProgram.cs
+++ NoiseAlertApp/NoiseAlertApp/MauiProgram.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.Extensions.Logging;
-
+using NoiseAlertApp.ViewModels;
+using Syncfusion.Maui.Core.Hosting;
 namespace NoiseAlertApp;
 
 public static class MauiProgram
@@ -9,14 +10,17 @@ public static class MauiProgram
 		var builder = MauiApp.CreateBuilder();
 		builder
 			.UseMauiApp<App>()
+			.ConfigureSyncfusionCore()
 			.ConfigureFonts(fonts =>
 			{
 				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
 				fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
 			});
+        builder.Services.AddSingleton<MainPage>();
+        builder.Services.AddSingleton<MainViewModel>();
 
 #if DEBUG
-		builder.Logging.AddDebug();
+        builder.Logging.AddDebug();
 #endif
 
 		return builder.Build();
---
+++ NoiseAlertApp/NoiseAlertApp/ViewModels/MainViewModel.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.Maui.Controls;
+using System;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using static System.Net.Mime.MediaTypeNames;
+using System.Collections.ObjectModel;
+using System.Windows.Input;
+
+namespace NoiseAlertApp.ViewModels
+{
+    public partial class MainViewModel : ObservableObject
+    {
+
+        public MainViewModel()
+        {
+            
+        }
+  
+        int clicked = 0;
+        [ObservableProperty]
+        double opacity = 0.9;
+
+        [ObservableProperty]
+        string buttonText = "Start";
+
+        [ObservableProperty]
+        double alertFreq = 1;
+
+        [ObservableProperty]
+        double noiseThreshold = 60;
+
+        [RelayCommand]
+        void Click()
+        {
+            clicked++;
+            if (clicked % 2 == 0)
+            {
+                ButtonText = "Start";
+                Opacity = 0.9;
+            }
+            else
+            {
+                ButtonText = "Stop";
+                Opacity = 0.0;
+            }
+        }
+    }
+}
+
